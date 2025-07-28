d700bd8d31442d620bd77b72cdd35ec15509234b
Maithili Mukherjee <86041684+maithilimukherjee@users.noreply.github.com>
2025-07-28 23:32:49 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-yellow-100">
+      <div className="text-orange-700 p-4 rounded">
+        your SPF is your BFF 😎
+      </div>
+    </div>
+  );
+}
